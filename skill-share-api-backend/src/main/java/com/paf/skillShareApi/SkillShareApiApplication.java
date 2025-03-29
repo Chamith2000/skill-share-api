119a1ebc513cb5a488c2de8052408a31234f43d7
Chamith2000 <chamithrathdunu38@gmail.com>
2025-03-29 16:36:58 +0530

    Create backend and frontend project

---
+++ skill-share-api-backend/src/main/java/com/paf/skillShareApi/SkillShareApiApplication.java
@@ -0,0 +1,13 @@
+package com.paf.skillShareApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillShareApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillShareApiApplication.class, args);
+	}
+
+}
