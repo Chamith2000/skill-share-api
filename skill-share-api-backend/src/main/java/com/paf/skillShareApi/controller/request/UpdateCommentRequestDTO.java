3277cddfe6fa65a099f5aa8cfe2b126810174d9b
TharukaJayawarna <jayawarnatharuka@gmail.com>
2025-05-07 12:50:39 +0530

    make createComment feature

---
+++ skill-share-api-backend/src/main/java/com/paf/skillShareApi/controller/request/UpdateCommentRequestDTO.java
@@ -0,0 +1,12 @@
+package com.paf.skillShareApi.controller.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class UpdateCommentRequestDTO {
+    @NotBlank(message = "Comment text cannot be blank")
+    @Size(min = 1, max = 500)
+    private String text;
+}
