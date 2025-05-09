8e12123550dc386f2e90be24772f7c2d316aeaa0
Chamith2000 <chamithrathdunu38@gmail.com>
2025-05-10 03:54:54 +0530

    bug fix

---
+++ skill-share-api-backend/src/main/java/com/paf/skillShareApi/controller/PostController.java
@@ -0,0 +1,49 @@
+package com.paf.skillShareApi.controller;
+
+import com.paf.skillShareApi.controller.request.CreatePostRequestDTO;
+import com.paf.skillShareApi.service.PostService;
+import jakarta.validation.Valid;
+import org.apache.tomcat.util.http.fileupload.FileUploadException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("/api")
+public class PostController {
+
+    @Autowired
+    private PostService postService;
+
+    @PostMapping("/users/{user-id}/posts")
+    public ResponseEntity<Map> createPost(
+            @ModelAttribute CreatePostRequestDTO postRequest,
+            @PathVariable("user-id") Long userId) {
+        return postService.createPost(postRequest, userId);
+    }
+
+    @GetMapping("/posts/{postId}")
+    public ResponseEntity<Map> getPost(@PathVariable Long postId) {
+        return postService.getPost(postId);
+    }
+
+    @GetMapping("/posts")
+    public ResponseEntity<Map> getAllPosts() {
+        return postService.getAllPosts();
+    }
+
+    @PutMapping("/posts/{postId}")
+    public ResponseEntity<Map> updatePost(@PathVariable Long postId, @Valid @RequestBody CreatePostRequestDTO postRequest) throws FileUploadException {
+        return postService.updatePost(postId, postRequest);
+    }
+
+    @DeleteMapping("/posts/{postId}")
+    public ResponseEntity<Map> deletePost(@PathVariable Long postId) {
+        return postService.deletePost(postId);
+    }
+}
---
+++ skill-share-api-backend/src/main/java/com/paf/skillShareApi/service/PostService.java
@@ -0,0 +1,14 @@
+package com.paf.skillShareApi.service;
+import com.paf.skillShareApi.controller.request.CreatePostRequestDTO;
+import org.apache.tomcat.util.http.fileupload.FileUploadException;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Map;
+
+public interface PostService {
+    public ResponseEntity<Map> createPost(CreatePostRequestDTO postRequest, Long userId);
+    public ResponseEntity<Map> getPost(Long postId);
+    ResponseEntity<Map> getAllPosts();
+    public ResponseEntity<Map> updatePost(Long postId, CreatePostRequestDTO postRequest) throws FileUploadException;
+    public ResponseEntity<Map> deletePost(Long postId);
+}
